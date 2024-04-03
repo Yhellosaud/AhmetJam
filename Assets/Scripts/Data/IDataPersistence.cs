67379d3e788d39c4b64519d3089675da3419ed7e
Ahmet Sariguney <ahmetsguney@gmail.com>
2024-04-04 00:13:03 +0300

    3_3_Ahmet

--- Assets/Scripts/Data/IDataPersistence.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDataPersistence
-{
-    void SaveData(GameData data);
-    void LoadData(GameData data);
-}
